e6fa3fa968db6ad03a8d38c6a431a8ab296f334d
BENJAMIN RESEK <bresek@clarku.edu>
2021-03-28 22:17:11 -0400

    basic operation display now works. Need to revise to handle multidigit inputs

--- index.js
+++ index.js
@@ -3,31 +3,39 @@ console.log('script connected')
 // simple math functions
 
 function add(num1,num2){
+  num1 = parseInt(num1)
+  num2 = parseInt(num2)
   return num1+num2
 }
 function subtract(num1,num2){
+  num1 = parseInt(num1)
+  num2 = parseInt(num2)
   return num1-num2
 }
 function multiply(num1,num2){
+  num1 = parseInt(num1)
+  num2 = parseInt(num2)
   return num1*num2
 }
 function divide(num1,num2){
+  num1 = parseInt(num1)
+  num2 = parseInt(num2)
   return num1/num2
 }
 
 //operate function- takes operation and two numerical inputs
 function operate(operation,num1,num2){
   switch (operation) {
-    case add:
+    case '+':
       return add(num1,num2)
       break;
-    case subtract:
+    case '-':
       return subtract(num1,num2)
       break;
-    case multiply:
+    case "x":
       return multiply(num1,num2)
       break;
-    case divide:
+    case "÷":
       return divide(num1,num2)
       break;
     default:
@@ -35,14 +43,36 @@ function operate(operation,num1,num2){
   }
 }
 
-//display button clicks
+//display number clicks
 
 buttons = document.querySelectorAll('button')
+let inputs = []
+let operator = ''
 
 buttons.forEach((button)=>{
   button.addEventListener("click", function(event){
-    displayValue = event.target.innerText
-    display(displayValue)
+    if (event.target.classList.contains('num')){
+      inputs.push(event.target.innerText) //need to change this to handle multidigit number
+      let displayValue = inputs[inputs.length-1]
+      display(displayValue)
+    }else if (event.target.classList.contains('operator')){
+      operator = event.target.innerText
+    } else if (event.target.classList.contains('clear')){
+      inputs = []
+      operator = ''
+      display('')
+    }
+    if (inputs.length >1){
+      console.log(inputs[inputs.length-2])
+      console.log(operator)
+      console.log(inputs[inputs.length-1])
+      result = operate(operator,inputs[inputs.length-2],inputs[inputs.length-1] )
+      display(result)
+      inputs = []
+      operator = ''
+      inputs.push(result)
+    }
+
   })
 })
 
